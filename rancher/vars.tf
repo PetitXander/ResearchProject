cb2eb00d8892db498f06fa02eb8bb554949f2e72
xander petit <xander.petit@student.howest.be>
2022-01-20 10:16:18 +0100

    test provider

---
+++ rancher/vars.tf
@@ -0,0 +1,24 @@
+variable "rancher_access_key" {
+  type = string
+  sensitive = true
+}
+
+variable "rancher_secret_key" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_sub" {
+  type = string
+  sensitive = true
+}
