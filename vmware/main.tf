c23d97017cabf533c11cc87cd19f8db370bec8e3
xander petit <xander.petit@student.howest.be>
2022-02-01 14:00:01 +0100

    Added vars

--- vmware/main.tf
+++ vmware/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "rancher2" {
-  api_url    = "https://192.168.50.104"
+  api_url    = var.rancher_url
   access_key = var.rancher_access_key
   secret_key = var.rancher_secret_key
   insecure = true
@@ -28,28 +28,28 @@ resource "rancher2_node_template" "vsphere_template" {
   cloud_credential_id = rancher2_cloud_credential.creds_vsphere.id
   engine_install_url = "https://releases.rancher.com/install-docker/20.10.sh"
   vsphere_config {
-    datacenter = "StudentDatacenter"
-    datastore = "petit-xander"
-    cpu_count = "2"
-    memory_size = "2048"
-    disk_size = "20000"
+    datacenter = var.datacenter
+    datastore = var.datastore
+    cpu_count = var.cpu_count
+    memory_size = var.memory_size
+    disk_size = var.disk_size
     network = ["VM Network"]
     cfgparam = ["disk.enableUUID=TRUE"]
     creation_type = "template"
-    clone_from = "ubuntu-focal-20.04-cloudimg"
+    clone_from = var.clone_from
 
   }
 }
 
 # Create a new rancher2 cluster template
 resource "rancher2_cluster_template" "cluster_template" {
-  name = "cluster_template"
+  name = var.cluster_temp_name
   template_revisions {
-    name = "V1"
+    name = var.cluster_temp_revision
     cluster_config {
       rke_config {
-        kubernetes_version = "v1.21.8-rancher1-1"
-        ignore_docker_version = false
+        kubernetes_version = var.kubernetes_version
+        ignore_docker_version = var.ignore_docker_version
         network {
           plugin = "canal"
         }
@@ -57,24 +57,24 @@ resource "rancher2_cluster_template" "cluster_template" {
     }
     default = true
   }
-  description = "cluster template"
+  description = var.cluster_temp_description
 }
 
 resource "rancher2_cluster" "cluster" {
-  name = "cluster"
+  name = var.cluster_name
   cluster_template_id = rancher2_cluster_template.cluster_template.id
   cluster_template_revision_id = rancher2_cluster_template.cluster_template.template_revisions.0.id
 }
 
 resource "rancher2_node_pool" "node_pool" {
   cluster_id       = rancher2_cluster.cluster.id
-  hostname_prefix  = "terraform"
-  name             = "nodepool"
+  hostname_prefix  = var.hostname_prefix
+  name             = var.nodepool_name
   node_template_id = rancher2_node_template.vsphere_template.id
-  quantity = 1
-  control_plane = true
-  etcd = true
-  worker = true
-  #TODO
+  quantity = var.nodepool_quantity
+  control_plane = var.control_plane
+  etcd = var.etcd
+  worker = var.worker
+
 }
 
--- vmware/vars.tf
+++ vmware/vars.tf
@@ -8,6 +8,11 @@ variable "rancher_secret_key" {
   sensitive = true
 }
 
+variable "rancher_url" {
+  type = string
+  sensitive = true
+}
+
 variable "vsphere_user" {
   type = string
   sensitive = true
@@ -22,4 +27,104 @@ variable "vsphere_ip" {
   type = string
   sensitive = true
 }
+
+## NODE TEMPLATE
+
+## MUST CHANGE
+variable "datacenter" {
+  type = string
+  default = "StudentDatacenter"
+}
+
+variable "datastore" {
+  type = string
+  default = "petit-xander"
+}
+
+## MUST CHANGE ^^
+
+variable "cpu_count" {
+  type = string
+  default = "2"
+}
+
+variable "memory_size" {
+  type = string
+  default = "2048"
+}
+
+variable "disk_size" {
+  type = string
+  default = "20000"
+}
+
+variable "clone_from" {
+  type = string
+  default = "ubuntu-focal-20.04-cloudimg"
+}
+
+## CLUSTER TEMPLATE
+variable "cluster_temp_name" {
+  type = string
+  default = "cluster_template"
+}
+
+variable "cluster_temp_revision" {
+  type = string
+  default = "V1"
+}
+
+variable "cluster_temp_description" {
+  type = string
+  default = "cluster template"
+}
+
+variable "kubernetes_version" {
+  type = string
+  default = "v1.21.8-rancher1-1"
+}
+
+variable "ignore_docker_version" {
+  type = string
+  default = "false"
+}
+
+## CLUSTER
+
+variable "cluster_name" {
+  type = string
+  default = "cluster"
+}
+
+## NODE POOL
+variable "hostname_prefix" {
+  type = string
+  default = "terraform"
+}
+
+variable "nodepool_name" {
+  type = string
+  default = "nodepool"
+}
+
+variable "nodepool_quantity" {
+  type = string
+  default = "1"
+}
+variable "control_plane" {
+  type = string
+  default = "true"
+}
+
+variable "etcd" {
+  type = string
+  default = "true"
+}
+
+variable "worker" {
+  type = string
+  default = "true"
+}
+
+
 
